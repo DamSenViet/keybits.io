64f64d21be694394e5d99ba453ae5d94b50a0311
Viet Tran <26016342+DamSenViet@users.noreply.github.com>
2023-04-23 20:22:46 -0700

    core: fix Favicon component file name
    - adjusted casing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import createEmotionCache from '@/utils/createEmotionCache';
-import Favicon from '@/components/favicon';
+import Favicon from '@/components/Favicon';
 export default class MyDocument extends Document {
   render() {
     return (
