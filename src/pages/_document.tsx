fae688491b93b147120c440d790f7795dc4ee194
Viet Tran <26016342+DamSenViet@users.noreply.github.com>
2023-04-23 17:55:53 -0700

    core: updated favicon
    - deleted default favicon
    
    - separated favicon tags into separate component
    
    - moved favicon tag to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import createEmotionCache from '@/utils/createEmotionCache';
-
+import Favicon from '@/components/favicon';
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <Favicon />
+        </Head>
         <body>
           <Main />
           <NextScript />
