fd1d0131a8c73876d1c2a21532ce9019052413e8
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-04-25 23:55:05 -0700

    feat: Rubik as default font w/Roboto fallback

--- src/theme/light.ts
+++ src/theme/light.ts
@@ -1,9 +1,33 @@
 import { createTheme } from '@mui/material/styles';
+import fonts from '@/fonts';
+
+const defaultTheme = createTheme();
+
+const fontFamily = fonts
+  .map(font => font.style.fontFamily)
+  .join(',');
+
+function createFontFamily(fontFamily: string) {
+  return {
+    fontFamily,
+    h1: { fontFamily },
+    h2: { fontFamily },
+    h3: { fontFamily },
+    h4: { fontFamily },
+    h5: { fontFamily },
+    h6: { fontFamily },
+    subtitle1: { fontFamily },
+    subtitle2: { fontFamily },
+    body1: { fontFamily },
+    body2: { fontFamily },
+    button: { fontFamily },
+    caption: { fontFamily },
+    overline: { fontFamily },
+  };
+};
 
 const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
+  typography: createFontFamily(fontFamily),
 });
 
 export default lightTheme;
