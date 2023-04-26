de7abd56054f0fe1b2670ac064346ea4f6e0c211
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-04-25 23:27:02 -0700

    fix: configured MUI deps to official example
    Copied MUI's Next.js (Typescript ver) configuration for _app, _document,
    and index pages.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,31 @@
 import * as React from 'react';
-import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import createEmotionCache from '@/utils/createEmotionCache';
 import lightTheme from '@/theme/light';
-// import '../styles/globals.css';
+import createEmotionCache from '@/utils/createEmotionCache';
 
-interface MyAppProps extends AppProps {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
-const clientSideEmotionCache = createEmotionCache();
-
-const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
-  const {
-    Component,
-    emotionCache = clientSideEmotionCache,
-    pageProps,
-  } = props;
-
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
       <ThemeProvider theme={lightTheme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
   );
-};
-
-export default MyApp;
+}
