476bb0f0b8708d65e2e8e6a24fbed1f3a358e1a4
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-04-29 20:15:37 -0700

    feat: adjusted home precursor section alignment

--- src/components/home/PrecursorPreview.tsx
+++ src/components/home/PrecursorPreview.tsx
@@ -4,8 +4,8 @@ import Typography from '@mui/material/Typography'
 
 const PrecursorPreview = () => (
   <Container sx={{ my: 25 }}>
-    <Typography variant="h2">Tools for Community</Typography>
-    <Typography variant="subtitle1" sx={{ my: 3 }}>
+    <Typography variant="h2" textAlign="center">Tools for Community</Typography>
+    <Typography variant="subtitle1" sx={{ my: 3 }} textAlign="center">
       These are the resources we have spent months developing.
       <br />
       We hope that you continue to enjoy our products.
