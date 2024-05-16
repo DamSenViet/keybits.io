fa64851c28b897cdd6c34cc3f68ad3dd7817b857
Viet <26016342+DamSenViet@users.noreply.github.com>
2024-05-15 21:57:00 -0700

    feat: added withDefaults hoc

---
+++ src/hocs/withDefaults.tsx
@@ -0,0 +1,25 @@
+import React, { Component, FunctionComponent } from 'react'
+import { isFunction } from 'lodash'
+
+function withDefaultProps<Props>(
+  BaseComponent: typeof Component<Props>,
+  defaultProps: Partial<Props> | ((props: Props) => Partial<Props>)
+): FunctionComponent<Props>
+function withDefaultProps<Props extends object>(
+  BaseComponent: FunctionComponent<Props>,
+  defaultProps: Partial<Props> | ((props: Props) => Partial<Props>)
+): FunctionComponent<Props>
+function withDefaultProps<Props extends object>(
+  BaseComponent: FunctionComponent<Props> | typeof Component<Props>,
+  defaultProps: Partial<Props> | ((props: Props) => Partial<Props>)
+): FunctionComponent<Props> {
+  return (props: Props) => {
+    const computedDefaultProps = React.useMemo(
+      () => (isFunction(defaultProps) ? defaultProps(props) : defaultProps),
+      [props]
+    )
+    return <BaseComponent {...props} {...computedDefaultProps} />
+  }
+}
+
+export default withDefaultProps
