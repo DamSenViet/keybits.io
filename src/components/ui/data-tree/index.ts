60f19213133a1e6bd76e012560764af5c5394038
Viet <26016342+DamSenViet@users.noreply.github.com>
2024-05-26 11:07:15 -0700

    chore: remove data tree

--- src/components/ui/data-tree/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Tree, type TreeProps } from './Tree'
-export { default as TreeItem, type TreeItemProps } from './TreeItem'
-export { default as TreeBranch, type TreeBranchProps } from './TreeBranch'
-export {
-  default as TreeItemTitle,
-  type TreeItemTitleProps,
-} from './TreeItemTitle'
