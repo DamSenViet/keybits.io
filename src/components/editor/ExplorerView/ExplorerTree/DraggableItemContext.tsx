99d9311e002e43dd0632c2a56f4f8094f6a8e703
Viet <26016342+DamSenViet@users.noreply.github.com>
2024-05-27 23:24:05 -0700

    chore: remove unused draggable item context

--- src/components/editor/ExplorerView/ExplorerTree/DraggableItemContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import { useDraggable } from '@dnd-kit/core'
-
-type DraggableItemContext = ReturnType<typeof useDraggable> | null
-
-const draggableItemContext = createContext<DraggableItemContext | null>(null)
-
-export default draggableItemContext
