66e9b458a306d539fbb3a5b633eb7bd5781cfc96
Viet <26016342+DamSenViet@users.noreply.github.com>
2024-06-14 23:31:24 -0700

    feat: scaffold editor toolbar menus

--- src/features/editor/components/Toolbar.tsx
+++ src/features/editor/components/Toolbar.tsx
@@ -1,5 +1,18 @@
 import { forwardRef } from 'react'
-import { Hammer, ZoomInIcon } from 'lucide-react'
+import { Hammer, Hand, MousePointer2, Square, ZoomInIcon } from 'lucide-react'
+import {
+  Menubar,
+  MenubarMenu,
+  MenubarItem,
+  MenubarShortcut,
+  MenubarTrigger,
+  MenubarContent,
+  MenubarSeparator,
+  MenubarSub,
+  MenubarSubTrigger,
+  MenubarSubContent,
+  MenubarCheckboxItem,
+} from '@/components/ui/menubar'
 
 export interface ToolbarProps {}
 
@@ -9,9 +22,113 @@ const Toolbar = forwardRef<HTMLDivElement, ToolbarProps>(function (props, ref) {
       ref={ref}
       className="h-12 border-b border-border flex justify-start items-center"
     >
-      <div className="px-4">
-        <Hammer className="stroke-1" />
-      </div>
+      <Menubar className="border-none rounded-none p-0 space-x-0">
+        {/* titlebar actions */}
+        <MenubarMenu>
+          <MenubarTrigger className="h-12 rounded-none ">
+            <Hammer className="stroke-1" />
+          </MenubarTrigger>
+          <MenubarContent>
+            <MenubarSub>
+              <MenubarSubTrigger>File</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarItem>
+                  New <MenubarShortcut>⌘N</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  Import <MenubarShortcut>⌘O</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  Export <MenubarShortcut>⌘S</MenubarShortcut>
+                </MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
+            <MenubarSub>
+              <MenubarSubTrigger>Edit</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarItem>
+                  Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  Redo <MenubarShortcut>⌘Y</MenubarShortcut>
+                </MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
+            <MenubarSub>
+              <MenubarSubTrigger>View</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarCheckboxItem>
+                  Grid <MenubarShortcut>⇧'</MenubarShortcut>
+                </MenubarCheckboxItem>
+                <MenubarCheckboxItem>
+                  Rulers <MenubarShortcut>⇧R</MenubarShortcut>
+                </MenubarCheckboxItem>
+                <MenubarSeparator />
+                <MenubarItem inset>
+                  Zoom In <MenubarShortcut>⌘+</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem inset>
+                  Zoom to 100% <MenubarShortcut>⌘-</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem inset>
+                  Zoom to fit <MenubarShortcut>⇧1</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem inset>
+                  Zoom to selection{' '}
+                  <MenubarShortcut className="ml-4">⇧2</MenubarShortcut>
+                </MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
+            <MenubarSub>
+              <MenubarSubTrigger>Selection</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarItem>
+                  Select All
+                  <MenubarShortcut className="ml-4">⌘A</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  Deselect All
+                  <MenubarShortcut className="ml-4">⇧⌘A</MenubarShortcut>
+                </MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
+            <MenubarSeparator />
+            <MenubarItem>Settings</MenubarItem>
+            <MenubarItem>Help</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Back to home page</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+
+        {/* select tool actions */}
+        <MenubarMenu>
+          <MenubarTrigger className="h-12 rounded-none">
+            <MousePointer2 className="stroke-1" />
+          </MenubarTrigger>
+        </MenubarMenu>
+
+        {/* create item actions */}
+        <MenubarMenu>
+          <MenubarTrigger className="h-12 rounded-none">
+            <Square className="stroke-1 stroke-white" />
+          </MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              Standard <MenubarShortcut>R</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>Stabilized</MenubarItem>
+            <MenubarItem>More...</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+
+        {/* panning tool */}
+        <MenubarMenu>
+          <MenubarTrigger className="h-12 rounded-none">
+            <Hand className="stroke-1 stroke-white" />
+          </MenubarTrigger>
+        </MenubarMenu>
+      </Menubar>
+
       <div className="font-bold text-sm ml-auto mr-auto">{'Keyboard Name'}</div>
       <div>
         <ZoomInIcon className="px-4" />
