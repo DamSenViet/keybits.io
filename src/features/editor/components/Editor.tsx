aed769d60475ca05fc4c55e65b5303f60345a97d
Viet <26016342+DamSenViet@users.noreply.github.com>
2024-07-05 01:02:31 -0700

    feat: simplify panel size

--- src/features/editor/components/Editor.tsx
+++ src/features/editor/components/Editor.tsx
@@ -1,12 +1,7 @@
 'use client'
 
-import { forwardRef, useLayoutEffect, useState, useId, useRef } from 'react'
-import {
-  ImperativePanelHandle,
-  Panel,
-  PanelGroup,
-  PanelResizeHandle,
-} from 'react-resizable-panels'
+import { forwardRef, useState } from 'react'
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
 import ExplorerSection from './ExplorerSection'
 import GraphSection from './GraphSection'
 import Toolbar from './Toolbar'
@@ -14,50 +9,7 @@ import { ClusterDetails, KeyDetails, KeyboardDetails } from './details'
 
 export interface EditorProps {}
 
-const MIN_LEFT_PANEL_WIDTH = 170
-
 const Editor = forwardRef<HTMLDivElement, EditorProps>(function (props, ref) {
-  // determines activity content to display in the left panel
-  const [activity, setActivity] = useState<string>('explorer')
-
-  const panelHandle = useRef<ImperativePanelHandle>(null)
-  const rightPanelWidth = 256
-  const resizeHandleWidth = 1
-  const panelGroupWidth = (window?.innerWidth || 1) - rightPanelWidth
-  const panelGroupContentWidth = panelGroupWidth - resizeHandleWidth
-  // 'Size' is a percentage
-  const defaultLeftPanelMinSize =
-    (MIN_LEFT_PANEL_WIDTH / panelGroupContentWidth) * 100
-  const [leftPanelMinSize, setLeftPanelMinSize] = useState(
-    defaultLeftPanelMinSize
-  )
-
-  const panelGroupId = useId()
-  // useLayoutEffect(() => {
-  //   const panelGroupEl = document.querySelector<HTMLDivElement>(
-  //     `[data-panel-group-id="${panelGroupId}"]`
-  //   )!
-
-  //   const observer = new ResizeObserver(() => {
-  //     setLeftPanelMinSize(
-  //       (MIN_LEFT_PANEL_WIDTH /
-  //         (panelGroupEl.offsetWidth - resizeHandleWidth)) *
-  //         100
-  //     )
-
-  //     const currSize = panelHandle.current?.getSize()!
-  //     console.log(currSize)
-  //     panelHandle.current?.resize(currSize)
-  //   })
-
-  //   // TODO FORCE PANEL TO PERSIST DIMENSIONS BETWEEN WINDOW SIZE CHANGES
-
-  //   observer.observe(panelGroupEl)
-  //   return () => {
-  //     observer.disconnect()
-  //   }
-  // }, [])
-
   // determines the settings editor for last focused item (or keyboard)
   const [lastFocused, setLastFocused] = useState<null>(null)
 
@@ -71,7 +23,6 @@ const Editor = forwardRef<HTMLDivElement, EditorProps>(function (props, ref) {
       <Toolbar />
       <div className="grow relative h-full flex flex-row items-stretch content-stretch">
         <PanelGroup
-          id={panelGroupId}
           className="grow"
           direction="horizontal"
           style={{ height: undefined, width: undefined }}
@@ -79,15 +30,14 @@ const Editor = forwardRef<HTMLDivElement, EditorProps>(function (props, ref) {
           {/* left panel */}
           <Panel
             className="flex flex-col overflow-hidden"
-            defaultSize={defaultLeftPanelMinSize}
-            minSize={leftPanelMinSize}
-            ref={panelHandle}
+            defaultSize={20}
+            minSize={20}
           >
             <ExplorerSection />
           </Panel>
           <PanelResizeHandle className="w-px border-r border-border rounded-full data-[resize-handle-state=hover]:bg-border/50 data-[resize-handle-state=drag]:bg-border/50" />
           {/* center panel */}
-          <Panel defaultSize={100}>
+          <Panel defaultSize={80}>
             <GraphSection />
           </Panel>
         </PanelGroup>
