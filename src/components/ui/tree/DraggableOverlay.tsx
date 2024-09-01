3767eaa86a99d6337975f9acafa9e0c5353e9200
Viet <26016342+DamSenViet@users.noreply.github.com>
2024-08-31 22:34:01 -0700

    fix: tree overlay rendering 0

--- src/components/ui/tree/DraggableOverlay.tsx
+++ src/components/ui/tree/DraggableOverlay.tsx
@@ -1,4 +1,3 @@
-import { Key, ReactNode } from 'react'
 import { createPortal } from 'react-dom'
 import { TreeNode } from './types/ui'
 
@@ -10,7 +9,7 @@ export default function DraggableOverlay({ items }: DraggableOverlayProps) {
   return (
     <>
       {createPortal(
-        items ? (
+        items && items.length ? (
           <div className="">{items.length ? items[0].label : items.length}</div>
         ) : null,
         document.body
